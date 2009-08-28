f79d8629e7d6935bda493868ce6fdea37261d7e1
Michael Schade <michaelschade@sddomain.com>
2009-08-28 01:23:09 -0500

    Added code to strip CDATA, reduced amount of parsing, and began coding to find each conversation ID to print out the SMS content.

--- smsgv.py
+++ smsgv.py
@@ -1,10 +1,12 @@
 from settings import USER, PASS # For testing purposes
 
-LOGIN_URL = "https://www.google.com/accounts/ServiceLoginAuth?service=grandcentral"
-MARK_READ_URL = "https://www.google.com/voice/m/mark?read=1&id="
-SMSLIST_URL = "https://www.google.com/voice/inbox/recent/sms" # We use the main website because Google includes helpful data stored via JSON here
-SMSLIST_M_URL = "https://www.google.com/voice/m/i/sms"
-SMS_SEND_URL = "https://www.google.com/voice/m/sendsms"
+# We use the mobile website for everything possible to save on bandwidth
+LOGIN_URL       = "https://www.google.com/accounts/ServiceLoginAuth?service=grandcentral"
+SMS_SEND_URL    = "https://www.google.com/voice/m/sendsms"
+MARK_READ_URL   = "https://www.google.com/voice/m/mark?read=1&id="
+SMSLIST_M_URL   = "https://www.google.com/voice/m/i/sms"
+# We use the main website (instead of mobile) because Google includes helpful data stored via JSON here
+SMSLIST_URL     = "https://www.google.com/voice/inbox/recent/sms"
 
 from re import compile
 timeConstrain = compile("now|\d{1,2} (seconds?|minutes?) ago|(1?\d|2[0-4]) hours? ago")
@@ -15,13 +17,12 @@ class GVAccount:
     """A representation of the user to handle their login, cookies, and message sending and receiving."""
     def __init__(self, username, password):
         self.last_time = 0 # A failsafe in case no other message can set the baseline.
-        self.uid = None # Google has a special little id that they require is included in sending messages
         self.logged_in = False
         self.initialized = False
         print "Logging in..."
         self.login(username, password)
     
-    def __markRead(self, conversation_ids):
+    def __mark_read(self, conversation_ids):
         # Each conversation has its own id, so we can pass that via GET to mark it as read.
         """ Takes a list of conversation ids and makes GET requests to Google's
         httpd to mark them as read conversations."""
@@ -30,14 +31,21 @@ class GVAccount:
             print "Marking %s as read" % cid
             urlopen(Request(MARK_READ_URL + cid))
     
-    def __smsParse(self, sms_page):
+    def __find_account_id(self):
+        """Finds account ID used by Google to authenticate SMS sending."""
+        # Google has a special little id that they require is included in sending messages
+        from urllib2 import Request, urlopen
+        handle = urlopen(Request(SMSLIST_M_URL))
+        from lxml import html
+        parsed_html = html.fromstring(handle.read())
+        # Grab UID from the page and set it.
+        self.uid = parsed_html.forms[0].inputs["_rnr_se"].value
+    
+    def __id_gather(self, parsed_html):
         """Parses the HTML received from Google's servers to receive only the relevant messages."""
-        from lxml import etree, html
-        parser = etree.XMLParser(strip_cdata=False) # Google likes to store their information in CDATA, so we have to tell the parser this
-        parsed_html = html.document_fromstring(sms_page.read(), parser)
-        message_ids = {}
+        message_ids = set()
         import cjson
-        json = cjson.decode(parsed_html.find('.json').text)
+        json = cjson.decode(parsed_html.find('.//json').text)
         # We only want to get relevant conversations
         for key, value in json['messages'].iteritems():
             # Check if unread and within the past twenty-four hours
@@ -47,20 +55,35 @@ class GVAccount:
             # Checks for multiple things:
             #   - Message is a SMS in the inbox
             #   - Message is unread
-            #   - Message is within the past 24 hours
+            #   - Message is within the past 24 hours (86,400 seconds)
             if not value['isRead'] and (time() - value['startTime'] < 86400) and ('sms' in value['labels']) and ('inbox' in value['labels']):
                 # If not initialized, then the -very- last message sent is
                 # found. This is used when later detecting new messages.
                 if int(value['startTime']) > self.last_time:
                     self.last_time = value['startTime']
                     if self.initialized:
-                        if key not in message_ids:
-                            pass # Should not need to do anything if id already indexed
-                        else:
-                            # Later switch to tuple to store phone number and conversation notes?
-                            message_ids[key] = value['phoneNumber']
+                        message_ids.add(key)
                         print "Message ID: %s" % key
+                        # if key not in message_ids:
+                        #                             pass # Should not need to do anything if id already indexed
+                        #                         else:
+                        #                             # Later switch to tuple to store phone number and conversation notes?
+                        #                             message_ids[key] = value['phoneNumber']
+                        #                         print "Message ID: %s" % key
         if not self.initialized: self.initialized = True
+        return message_ids
+    
+    def __sms_parse(self, sms_page):
+        from lxml import etree, html
+        # Strip CDATA tags
+        html_page = sms_page.read()
+        html_page = html_page.replace('<![CDATA[', '')
+        html_page = html_page.replace(']]>', '')
+        parsed_html = html.document_fromstring(html_page)
+        message_ids = self.__id_gather(parsed_html) # Also sets initialized to true if necessary
+        for cid in message_ids:
+            conversation = parsed_html.find_class('gc-message-sms')[0].getparent().get_element_by_id(cid).find_class('gc-message-message-display')
+            print conversation[0].text_content()
         return message_ids
     
     def login(self, username, password):
@@ -78,26 +101,17 @@ class GVAccount:
                 'continue': 'https://www.google.com/voice/m/i/sms/',
                 'Email': username,
                 'Passwd': password,
-                'PersistentCookies': 'yes',
+                'PersistentCookies': 'yes', # Keeps the account logged in.
             })
             urlopen(Request(LOGIN_URL, form, {'Content-type': 'application/x-www-form-urlencoded'}))
             self.logged_in = True
             self.__find_account_id()
     
-    def __find_account_id(self):
-        if self.uid is None:
-            from urllib2 import Request, urlopen
-            handle = urlopen(Request(SMSLIST_M_URL))
-            from lxml import html
-            parsed_html = html.fromstring(handle.read())
-            # Grab UID from the page and set it.
-            self.uid = parsed_html.forms[0].inputs["_rnr_se"].value
-    
     def sms_check(self):
         """Retrieves the SMS messages from Google's servers to pass to the Parse function."""
         from urllib2 import Request, urlopen
         handle = urlopen(Request(SMSLIST_URL))
-        self.__markRead(self.__smsParse(handle))
+        self.__mark_read(self.__sms_parse(handle))
     
     def sms_send(self, number, message):
         from urllib2 import Request, urlopen
@@ -111,5 +125,4 @@ class GVAccount:
 
 # Also for testing purposes
 x = GVAccount(USER, PASS)
-# from lxml import html
-# z = x.sms_check()
+x.sms_check()
